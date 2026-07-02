f1e97a24746d6f731fa0b635b730af4efe2905b0
Adrjola <adrijus.kontenis17@gmail.com>
2026-07-02 22:36:04 +0300

    Add REST controllers and exception handling

---
+++ src/main/java/com/demo/banking/app/team1/dto/ErrorResponseDto.java
@@ -0,0 +1,18 @@
+package com.demo.banking.app.team1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponseDto {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private List<String> messages;
+    private String path;
+}
