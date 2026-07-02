ffcbdaa5e3ea1ca3dc8818a0f7ee446452f3bf08
karina <karinasteina@gmail.com>
2026-07-02 20:38:47 +0300

    minor changes in the dto package

--- src/main/java/com/demo/banking/app/team1/dto/TransferResponseDto.java
+++ src/main/java/com/demo/banking/app/team1/dto/TransferResponseDto.java
@@ -1,5 +1,9 @@
 package com.demo.banking.app.team1.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +16,22 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransferResponseDto {
-    private long fromAccountId;
-    private long toAccountId;
-    private double amount;
-    private double fromAccountNewBalance;
-    private double toAccountNewBalance;
+
+    @NotNull(message = "From account ID is required")
+    private Long fromAccountId;
+
+    @NotNull(message = "To account ID is required")
+    private Long toAccountId;
+
+    @Positive(message = "Transfer amount must be greater than 0")
+    private Double amount;
+
+    @PositiveOrZero(message = "Balance amount must be greater than or equal to 0")
+    private Double fromAccountNewBalance;
+
+    @PositiveOrZero(message = "Balance amount must be greater than or equal to 0")
+    private Double toAccountNewBalance;
+
+    @PastOrPresent(message = "TimeStamp cannot be in the future")
     private LocalDateTime timeStamp;
 }
