b3bb0f8fde80501ddee8c11acf7e2328b5a3b582
karinasteina <91082933+karinasteina@users.noreply.github.com>
2026-07-02 21:13:17 +0300

    Update TransferRequestDto.java

--- src/main/java/com/demo/banking/app/team1/dto/TransferRequestDto.java
+++ src/main/java/com/demo/banking/app/team1/dto/TransferRequestDto.java
@@ -21,7 +21,7 @@ public class TransferRequestDto {
     private Long toAccountId;
 
     @Positive(message = "Transfer amount must be greater than 0")
-    private Double amount;
+    private double amount;
 
     @NotBlank(message = "Note cannot be blank")
     private String note;
