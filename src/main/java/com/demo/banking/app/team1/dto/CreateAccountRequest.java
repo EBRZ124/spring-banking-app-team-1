4878556b45775f6bd210f8e889028040e721eab2
Zeynep Aizpurvs <zey.ozdemir21@outlook.com>
2026-07-02 18:16:04 +0300

    Created CreateAccountRequest and TransferRequest DTOs with input validation

--- src/main/java/com/demo/banking/app/team1/dto/CreateAccountRequest.java
+++ src/main/java/com/demo/banking/app/team1/dto/CreateAccountRequest.java
@@ -1,8 +1,26 @@
 package com.demo.banking.app.team1.dto;
-import lombok.Data;
-@Data
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
 public class CreateAccountRequest
 {
+    @NotBlank(message = "IBAN cannot be empty.")
+    private String iban;
+
+    @NotBlank(message = "Owner name cannot be empty.")
+    private String ownerName;
+
+    @PositiveOrZero(message = "Balance cannot be negative.")
+    private double balance;
 
 
 }
