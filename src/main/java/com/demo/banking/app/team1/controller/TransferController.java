27e51ee40e4e7a88cc9cb51f5e13c134f8a20515
martHaza <martazapacka17@gmail.com>
2026-07-03 09:56:30 +0300

    Added logging to accountController and transferController

--- src/main/java/com/demo/banking/app/team1/controller/TransferController.java
+++ src/main/java/com/demo/banking/app/team1/controller/TransferController.java
@@ -5,6 +5,7 @@ import com.demo.banking.app.team1.dto.TransferResponseDto;
 import com.demo.banking.app.team1.service.TransferService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/transfer")
@@ -21,6 +23,11 @@ public class TransferController {
 
     @PostMapping
     public ResponseEntity<TransferResponseDto> createTransfer(@Valid @RequestBody TransferRequestDto request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(transferService.transfer(request));
+        log.info("Received transfer request: {}", request);
+
+        TransferResponseDto response = transferService.transfer(request);
+
+        log.info("Transfer completed successfully: {}", response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
--- src/main/java/com/demo/banking/app/team1/dto/TransferResponseDto.java
+++ src/main/java/com/demo/banking/app/team1/dto/TransferResponseDto.java
@@ -8,11 +8,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransferResponseDto {
